ef6c358334c1b20c20c4f5480a4410c0f135b19a
Dmitriy Baranov <bdvbuild@gmail.com>
2023-01-31 15:37:47 +0400

    Add project files.

---
+++ RevitAPITraining_WriteToTextFile/Main.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace RevitAPITraining_WriteToTextFile
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string roomInfo = string.Empty;
+
+            var rooms = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .Cast<Room>()
+                .ToList();
+
+            foreach (var room in rooms)
+            {
+                string roomName = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString();
+                roomInfo += $"{roomName}\t{room.Number}\t{room.Area}{Environment.NewLine}";
+            }
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string csvPath = Path.Combine(desktopPath, "roomInfo.csv");
+
+            File.WriteAllText(csvPath, roomInfo);
+
+            return Result.Succeeded;
+        }
+    }
+}
